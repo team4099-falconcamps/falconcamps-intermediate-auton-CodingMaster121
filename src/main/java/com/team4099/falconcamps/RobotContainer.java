8dff8374ff0f05058d0a5ff197b08d1eff181b2b
CodingMaster121 <ytryanlo@gmail.com>
2020-07-15 19:01:12 +0000

    Added Autonomous for shooterCommand

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -1,18 +1,21 @@
 package com.team4099.falconcamps;
 
+import com.team4099.falconcamps.commands.ShootCommand;
 import com.team4099.falconcamps.subsystems.Drivetrain;
 import com.team4099.falconcamps.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 
 public class RobotContainer {
     public Drivetrain drivetrain = new Drivetrain();
     public Shooter shooter = new Shooter();
+    public ShootCommand shooterCommand = new ShootCommand(shooter);
 
     public RobotContainer() {
 
     }
 
     public Command getAutonomousCommand() {
-        return null;
+        return new ParallelRaceGroup(shooterCommand, shooterCommand.withTimeout(5.0));
     }
 }
