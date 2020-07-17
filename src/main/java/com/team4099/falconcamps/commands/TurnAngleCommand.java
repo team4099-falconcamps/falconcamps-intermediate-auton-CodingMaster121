878f5ecf57cb29799f1bea2094c84ea6a88b21c8
CodingMaster121 <ytryanlo@gmail.com>
2020-07-17 17:15:40 +0000

    Added Requirements for the Command

--- src/main/java/com/team4099/falconcamps/commands/TurnAngleCommand.java
+++ src/main/java/com/team4099/falconcamps/commands/TurnAngleCommand.java
@@ -13,6 +13,7 @@ public class TurnAngleCommand extends CommandBase {
     public TurnAngleCommand(double angleDegrees, Drivetrain drivetrain) {
         this.angleDegrees = angleDegrees;
         this.drivetrain = drivetrain;
+        this.addRequirements(drivetrain);
     }
 
     @Override
